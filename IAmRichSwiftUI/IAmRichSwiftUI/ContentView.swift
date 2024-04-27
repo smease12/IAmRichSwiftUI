8de0bbb7f847aa32b74b3f476b1d602263628d6b
smease12 <scmease93@gmail.com>
2024-04-27 19:09:27 -0400

    Add appicon and diamond to assets

--- IAmRichSwiftUI/IAmRichSwiftUI/ContentView.swift
+++ IAmRichSwiftUI/IAmRichSwiftUI/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(red: 0.5, green: 0.5, blue: 0.5)
-            Text("I am Rich").font(.system(size: 40))
-                .fontWeight(.bold)
+            Color(.systemTeal).edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I am Rich").font(.system(size: 40))
+                    .fontWeight(.bold)
                 .foregroundColor(Color.white)
+                Image("Image Name")
+            }
         }
     }
 }
