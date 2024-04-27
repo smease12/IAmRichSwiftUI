6b20537c2cefc16377b7cc72f5fd57b0992990ee
smease12 <scmease93@gmail.com>
2024-04-27 15:10:42 -0400

    Initial commit

---
+++ IAmRichSwiftUI/IAmRichSwiftUI/IAmRichSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichSwiftUIApp.swift
+//  IAmRichSwiftUI
+//
+//  Created by Serena  on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
